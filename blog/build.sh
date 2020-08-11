320d3f1788e76b8b58a98872686eb2ecbecff0e1
Henrique Gogó <henriquegogo@gmail.com>
2020-08-11 01:02:05 -0300

    Adding date

--- blog/build.sh
+++ blog/build.sh
@@ -3,8 +3,9 @@ cat _post.html > index.html
 for file in $(ls -r [0-9]*.html)
 do
   title=$(cat $file | grep "<title>" | sed "s/.title>//g" | sed "s/<$//")
+  date=$(cat $file | grep \"date | sed "s/^.*date\" content=\"//" | sed "s/\">//")
   if [ "$file" != "index.html" ] && [ "$file" != "_post.html" ]
   then
-    echo -n "<a href='$file'>$title</a><hr>" >> index.html
+    echo -n "<a href='$file'>$title</a> <small><i>($date)</i></small><hr>" >> index.html
   fi
 done
