c34686ab5cf541647788bf5c6e48bc533dbda04d
Henrique Gogó <henriquegogo@gmail.com>
2020-08-11 01:48:18 -0300

    Font as https

--- blog/index.js
+++ blog/index.js
@@ -11,7 +11,7 @@
     document.body.innerHTML = `
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
-        @import url('http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en');
+        @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en');
 
         body {
           margin: 0;
